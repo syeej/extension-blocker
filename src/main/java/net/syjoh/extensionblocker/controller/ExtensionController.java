c20448676d28c9bfccf32d4191178a69fe293e1e
syjoe <syee2joe@gmail.com>
2024-08-15 20:47:50 +0900

    feat: 컨트롤러 및 화면 구현

--- src/main/java/net/syjoh/extensionblocker/controller/ExtensionController.java
+++ src/main/java/net/syjoh/extensionblocker/controller/ExtensionController.java
@@ -2,12 +2,15 @@ package net.syjoh.extensionblocker.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.syjoh.extensionblocker.domain.FixedExtensionType;
+import net.syjoh.extensionblocker.dto.CustomExtensionDTO;
 import net.syjoh.extensionblocker.dto.FixedExtensionDTO;
 import net.syjoh.extensionblocker.service.ExtensionService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.HashMap;
 import java.util.List;
@@ -34,8 +37,26 @@ public class ExtensionController {
         return "extensions";
     }
     //고정 확장자 등록
-
+    @PostMapping("/fixed/{type}")
+    public String updateFixedExtension(@PathVariable FixedExtensionType type,
+                                       @RequestParam boolean isBlocked,
+                                       RedirectAttributes redirectAttributes) {
+        extensionService.updateFixedExtensionStatus(type, isBlocked);
+        redirectAttributes.addFlashAttribute("message", "udpate success");
+        return "redirect:/extensions";
+    }
     //커스텀 확장자 등록
-
+    @PostMapping("/custom")
+    @ResponseBody
+    public ResponseEntity<?> addCustomExtension(@RequestParam String name) {
+        CustomExtensionDTO newExtension = extensionService.addCustomExtension(name);
+        return ResponseEntity.ok(newExtension);
+    }
     //커스텀 확장자 삭제
+    @DeleteMapping("/custom/{id}")
+    @ResponseBody
+    public ResponseEntity<String> deleteCustomExtension(@PathVariable Long id) {
+        extensionService.deleteCustomExtension(id);
+        return ResponseEntity.ok("success");
+    }
 }
