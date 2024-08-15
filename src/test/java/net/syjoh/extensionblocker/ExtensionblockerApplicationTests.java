fb752051a3a4a1aef50f0f23ab9e35bc3f8e0805
syjoe <syee2joe@gmail.com>
2024-08-15 16:01:30 +0900

    init

---
+++ src/test/java/net/syjoh/extensionblocker/ExtensionblockerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.syjoh.extensionblocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtensionblockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
